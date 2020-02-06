1d78944c56044828baf4c9b766bc184e0d0382d3
Frauke <frauke_heedfeld@hotmail.com>
2020-02-06 14:04:22 +0100

    added firebase + login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,30 @@ import { HomeComponent } from './home/home.component';
 import { OverviewComponent } from './overview/overview.component';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { LoginComponent } from './admin/login/login.component';
+
 const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: 'home', component: HomeComponent },
 	{ path: 'overview', component: OverviewComponent }
 ];
 
+var config = {
+    apiKey: "AIzaSyDQ8BdsSU7VuIQzNblGwU83lnBvJpO3DEg",
+    authDomain: "eindproef-februari.firebaseapp.com",
+    databaseURL: "https://eindproef-februari.firebaseio.com",
+    projectId: "eindproef-februari",
+    storageBucket: "eindproef-februari.appspot.com",
+    messagingSenderId: "940164973892",
+    appId: "1:940164973892:web:627af097551d0301e44311"
+  };
+
 @NgModule({
-	declarations: [ AppComponent, HomeComponent, OverviewComponent ],
-	imports: [ BrowserModule, AppRoutingModule, RouterModule.forRoot(routes) ],
+	declarations: [ AppComponent, HomeComponent, OverviewComponent, LoginComponent ],
+	imports: [ BrowserModule, AppRoutingModule, RouterModule.forRoot(routes), AngularFireModule.initializeApp(config),
+		AngularFireAuthModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
