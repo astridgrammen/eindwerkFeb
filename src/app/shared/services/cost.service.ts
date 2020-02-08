e51e92e397c225a1a267c895736a1bc548880c64
Frauke <frauke_heedfeld@hotmail.com>
2020-02-08 11:47:03 +0100

    model import works

--- src/app/shared/services/cost.service.ts
+++ src/app/shared/services/cost.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
-//import { Costs } from './shared/model/costs.model' ;
+import { Costs } from '../model/costs.model' ;
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 //import { Costs} from './shared/model/costs.model';
+import { tap } from "rxjs/operators" ;
+import { Observable } from "rxjs" ;
 
 
 @Injectable ()
