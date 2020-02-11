f277e5bba589e90dce1cfc83fc557e82c0f79b25
Astrid Grammen <astrid.grammen@hotmail.com>
2020-02-11 15:22:56 +0100

    costs replace to hhome component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { CostService } from '../shared/services/cost.service';
 
-import { AuthService } from '../auth/auth.service.service' ;
-import { Router } from '@angular/router' ;
+import { Costs } from '../shared/model/costs.model';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent implements OnInit {
+	costs$: Observable<Costs[]>;
+	currentcost$: Observable<Costs>;
+	constructor(private router: Router, private authService: AuthService, private costService: CostService) {}
 
-  constructor(
-    private router: Router,
-    private authService: AuthService
-  ) { }
+	ngOnInit() {
+		if (!this.authService.isLoggedIn) {
+			this.router.navigate([ '/login' ]);
+		}
+		this.costs$ = this.costService.getCosts();
+	}
+	addCost(costDescription: string, costCost: number, costCategory: string, costType: string, costDate: string) {
+		// id === null, omdat deze auto wordt ingevuld door de json server
+		const newCost = new Costs(null, costDescription, costCost, costCategory, costType, costDate);
+		this.costService.addCost(newCost).subscribe((addedCost: Costs) => {
+			// countries opnieuw ophalen in de subscription
+			this.costs$ = this.costService.getCosts();
+		});
+	}
+	deleteCost(value) {
+		this.costService.deleteCost(value).subscribe();
+	}
 
-  ngOnInit() {
-    if ( !this.authService.isLoggedIn) { this.router.navigate ([ '/login' ])}
-  }
+	editCost(
+		costId: number,
+		costDescription: string,
+		costCost: number,
+		costCategory: string,
+		costType: string,
+		costDate: string
+	) {
+		const newCost = new Costs(costId, costDescription, costCost, costCategory, costType, costDate);
+		this.costService.editCost(newCost).subscribe((addedCost: Costs) => {
+			this.costs$ = this.costService.getCosts();
+		});
+	}
 }
--- src/app/shared/services/cost.service.ts
+++ src/app/shared/services/cost.service.ts
@@ -1,41 +1,39 @@
-import { Injectable } from "@angular/core";
-import { Costs } from '../model/costs.model' ;
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Costs } from '../model/costs.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { tap } from "rxjs/operators";
-import { Observable } from "rxjs";
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
-@Injectable ()
-
+@Injectable()
 export class CostService {
-    url = "http://localhost:3000/costs";
+	url = 'http://localhost:3000/costs';
 
-    constructor(private http: HttpClient) { }
-    // alle countries retourneren
-    getCosts(): Observable<Costs[]> {
-        return this.http
-            .get<Costs[]>(this.url)
-            //.pipe(tap(result => console.log("via json-server: ", result)));
-    }
-    /*
+	constructor(private http: HttpClient) {}
+	// alle countries retourneren
+	getCosts(): Observable<Costs[]> {
+		return this.http.get<Costs[]>(this.url);
+		//.pipe(tap(result => console.log("via json-server: ", result)));
+	}
+	/*
     getCost(id: number) {
         return this.http.get<Costs>(`${this.url}/${id}`);
     }
     */
-    addCost(newCost: Costs): Observable<any> {
-        const headers = new HttpHeaders().set("Content-type", "application/json");
-        return this.http.post(this.url, newCost, { headers: headers });
-    }
+	addCost(newCost: Costs): Observable<any> {
+		const headers = new HttpHeaders().set('Content-type', 'application/json');
+		return this.http.post(this.url, newCost, { headers: headers });
+	}
 
-  deleteCost(value){
-    return this.http.delete(this.url + "/" + value)
-  }
+	deleteCost(value) {
+		return this.http.delete(this.url + '/' + value);
+	}
 
-  editCost(newCost: Costs):Observable<any>{
-    //console.log(newCost.id);
-    const headers = new HttpHeaders().set("Content-type", "application/json");
-    return this.http.put(this.url + "/" + newCost.id, newCost, {
-      headers: headers
-    });
-  }
+	editCost(newCost: Costs): Observable<any> {
+		//console.log(newCost.id);
+		const headers = new HttpHeaders().set('Content-type', 'application/json');
+		return this.http.put(this.url + '/' + newCost.id, newCost, {
+			headers: headers
+		});
+	}
 }
