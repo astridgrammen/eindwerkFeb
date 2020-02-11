ac5cb0557b6c3c1a8050376d8c2da97a2c01f1a2
Astrid Grammen <astrid.grammen@hotmail.com>
2020-02-11 13:32:54 +0100

    Added date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { CostService } from './shared/services/cost.service' ;
+import { CostService } from './shared/services/cost.service';
 
-import { Costs } from './shared/model/costs.model' ;
+import { Costs } from './shared/model/costs.model';
 import { Observable } from 'rxjs';
 
-import { Router } from "@angular/router"; 
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
-
 export class AppComponent implements OnInit {
-  costs$: Observable<Costs[]>;
-  currentcost$: Observable<Costs>
-  constructor(private costService: CostService, private router: Router) { }
-  ngOnInit() {
-      this.costs$ = this.costService.getCosts();
-  }
+	costs$: Observable<Costs[]>;
+	currentcost$: Observable<Costs>;
+	constructor(private costService: CostService, private router: Router) {}
+	ngOnInit() {
+		this.costs$ = this.costService.getCosts();
+	}
 
-  addCost(costDescription: string, costCost: number, costCategory: string, costType: string) {
-      // id === null, omdat deze auto wordt ingevuld door de json server
-      const newCost = new Costs(null, costDescription, costCost, costCategory, costType);
-      this.costService.addCost(newCost)
-          .subscribe((addedCost: Costs) => {
-              // countries opnieuw ophalen in de subscription
-              this.costs$ = this.costService.getCosts();
-          });
-  }
-    deleteCost(value){
-        this.costService.deleteCost(value).subscribe();
-    }
+	addCost(costDescription: string, costCost: number, costCategory: string, costType: string, costDate: string) {
+		// id === null, omdat deze auto wordt ingevuld door de json server
+		const newCost = new Costs(null, costDescription, costCost, costCategory, costType, costDate);
+		this.costService.addCost(newCost).subscribe((addedCost: Costs) => {
+			// countries opnieuw ophalen in de subscription
+			this.costs$ = this.costService.getCosts();
+		});
+	}
+	deleteCost(value) {
+		this.costService.deleteCost(value).subscribe();
+	}
 
-    editCost(costId: number, costDescription: string, costCost: number, costCategory: string, costType:string) {
-        const newCost = new Costs(costId, costDescription, costCost, costCategory, costType);
-        this.costService.editCost(newCost)
-            .subscribe((addedCost: Costs) => {
-                this.costs$ = this.costService.getCosts();
-            });
-    }
-
+	editCost(
+		costId: number,
+		costDescription: string,
+		costCost: number,
+		costCategory: string,
+		costType: string,
+		costDate: string
+	) {
+		const newCost = new Costs(costId, costDescription, costCost, costCategory, costType, costDate);
+		this.costService.editCost(newCost).subscribe((addedCost: Costs) => {
+			this.costs$ = this.costService.getCosts();
+		});
+	}
 }
--- src/app/shared/model/costs.model.ts
+++ src/app/shared/model/costs.model.ts
@@ -1,10 +1,10 @@
 export class Costs {
-    constructor (
-    public id: number,
-    public description: string,
-    public cost: number,
-    public category: string,
-    public type: string
-
-    ) {}
+	constructor(
+		public id: number,
+		public description: string,
+		public cost: number,
+		public category: string,
+		public type: string,
+		public date: string
+	) {}
 }
