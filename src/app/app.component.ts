2ecea1286bc38fc42e09f0158557a9d04f7a21dd
Frauke <frauke_heedfeld@hotmail.com>
2020-02-17 10:00:25 +0100

    deleted unnecessary code app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CostService } from './shared/services/cost.service';
+
 import { AuthService } from './auth/auth.service.service';
 
-import { Costs } from './shared/model/costs.model';
-import { Observable } from 'rxjs';
 
 import { Router } from '@angular/router';
 
@@ -14,37 +12,9 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
 
-	costs$: Observable<Costs[]>;
-	currentcost$: Observable<Costs>;
-	constructor(private costService: CostService, private router: Router, private authService: AuthService) {}
+	constructor(private router: Router, private authService: AuthService) {}
 	ngOnInit() {
-		this.costs$ = this.costService.getCosts();
+
 		if ( !this.authService.isLoggedIn) { this.router.navigate ([ '/login' ])};
-	}
-
-	addCost(costDescription: string, costCost: number, costCategory: string, costType: string, costDate: string) {
-		// id === null, omdat deze auto wordt ingevuld door de json server
-		const newCost = new Costs(null, costDescription, costCost, costCategory, costType, costDate);
-		this.costService.addCost(newCost).subscribe((addedCost: Costs) => {
-
-			this.costs$ = this.costService.getCosts();
-		});
-	}
-	deleteCost(value) {
-		this.costService.deleteCost(value).subscribe();
-	}
-
-	editCost(
-		costId: number,
-		costDescription: string,
-		costCost: number,
-		costCategory: string,
-		costType: string,
-		costDate: string
-	) {
-		const newCost = new Costs(costId, costDescription, costCost, costCategory, costType, costDate);
-		this.costService.editCost(newCost).subscribe((addedCost: Costs) => {
-			this.costs$ = this.costService.getCosts();
-		});
 	}
 }
